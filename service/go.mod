38bcf5638e43f6de0e117b8fcf555ff0a4e23b56
Faizan Abbas <192166734+faizanabbas@users.noreply.github.com>
2024-12-28 21:45:23 +0000

    Generate short alias for shorten url response
    - Refactor response encoding to handle error

--- service/go.mod
+++ service/go.mod
@@ -3,3 +3,5 @@ module github.com/faizanabbas/aliasiq/service
 go 1.23
 
 require github.com/go-chi/chi/v5 v5.2.0
+
+require github.com/matoous/go-nanoid/v2 v2.1.0
--- service/handlers/shorten.go
+++ service/handlers/shorten.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	gonanoid "github.com/matoous/go-nanoid/v2"
 	"net/http"
 )
 
@@ -16,18 +17,26 @@ type response struct {
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
 	var request request
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
 		return
 	}
 
-	// TODO: Add logic to generate a short alias
+	alias, err := gonanoid.Generate("abcdefghijklmnopqrstuvwxyz0123456789", 7)
+	if err != nil {
+		http.Error(w, "error generating short alias", http.StatusInternalServerError)
+		return
+	}
 
 	// TODO: Store short alias in Redis
 
 	response := response{
-		ShortURL: "https://localhost:8080/redirect/" + "alias123",
+		ShortURL: "https://localhost:8080/redirect/" + alias,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "error encoding response", http.StatusInternalServerError)
+		return
+	}
 }
