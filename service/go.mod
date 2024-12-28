5b4d19f5f8be1d8c675f5f53111e7767bd24e7ee
Faizan Abbas <192166734+faizanabbas@users.noreply.github.com>
2024-12-28 18:01:39 +0000

    Initial commit

---
+++ service/go.mod
@@ -0,0 +1,5 @@
+module github.com/faizanabbas/aliasiq/service
+
+go 1.23
+
+require github.com/go-chi/chi/v5 v5.2.0
---
+++ service/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, from AliasIQ."))
+	})
+
+	http.ListenAndServe(":8080", r)
+}
