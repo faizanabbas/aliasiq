11c045e8a1ce919b4d851a9583bef47a00bbf123
Faizan Abbas <192166734+faizanabbas@users.noreply.github.com>
2024-12-28 19:15:53 +0000

    Accept request body containing url to shorten

--- service/handlers/shorten.go
+++ service/handlers/shorten.go
@@ -1,7 +1,33 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type request struct {
+	OriginalURL string `json:"originalUrl"`
+}
+
+type response struct {
+	ShortURL string `json:"shortUrl"`
+}
 
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Shorten URL"))
+	var request request
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: Add logic to generate a short alias
+
+	// TODO: Store short alias in Redis
+
+	response := response{
+		ShortURL: "https://localhost:8080/redirect/" + "alias123",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
