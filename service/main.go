e40a7100195af9131d3fe322c15dc5a5c108e508
Faizan Abbas <192166734+faizanabbas@users.noreply.github.com>
2024-12-28 18:13:10 +0000

    Add shorten, redirect and analytics routes to service

--- service/main.go
+++ service/main.go
@@ -16,5 +16,22 @@ func main() {
 		w.Write([]byte("Hello, from AliasIQ."))
 	})
 
+	r.Post("/shorten", shortenURLHandler)
+	r.Get("/redirect/{alias}", redirectURLHandler)
+	r.Get("/analytics/{alias}/", analyticsHandler)
+
 	http.ListenAndServe(":8080", r)
+}
+func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Shorten URL"))
+}
+
+func redirectURLHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Redirect URL"))
+}
+
+func analyticsHandler(w http.ResponseWriter, r *http.Request) {
+	alias := chi.URLParam(r, "alias")
+
+	w.Write([]byte("Analytics for alias: " + alias))
 }
